ac14013415935f0abcf5ea7472c1b9ecb3e406a7
dusty-nv <dustinf@nvidia.com>
2019-01-29 13:03:42 -0500

    save training labels

--- data/make_homography_sequence.cc
+++ data/make_homography_sequence.cc
@@ -22,7 +22,7 @@ namespace fs = std::experimental::filesystem;
 
 void readme()
 {
-	cout << "Usage: ./make_homography_sequence <dataset_directory> <sequence> <int_patch_size> <int_max_jitter> <n_samples> <bool_show_plots>" << endl;
+	cout << "Usage: ./make_homography_sequence <dataset_in_path> <scaled_dataset_out_path> <labels_out_path>" << endl;
 }
 
 
@@ -31,7 +31,7 @@ int main( int argc, char **argv )
 	cout << "OpenCV Version: " << CV_MAJOR_VERSION << ".";
 	cout << CV_MINOR_VERSION << endl;
 
-	if( argc < 2 )
+	if( argc < 4 )
 	{
 		readme();
 		return -1;
@@ -41,8 +41,14 @@ int main( int argc, char **argv )
 	 * parse command line arguments
 	 */
 	const std::string dataset_path(argv[1]);
+	const std::string dataset_out_path(argv[2]);
+	const std::string labels_path(argv[3]);
 
-	cout << "data path:  " << dataset_path << endl;
+	cout << "dataset input path:  " << dataset_path << endl;
+	cout << "scaled dataset output path:  " << dataset_out_path << endl;
+	cout << "training labels output path:  " << labels_path << endl;
+
+	std::ofstream labels_file(labels_path);
 
 
 	/*
@@ -111,8 +117,8 @@ int main( int argc, char **argv )
 		sprintf(img1_path, dataset_path.c_str(), img1_index);
 		sprintf(img2_path, dataset_path.c_str(), img2_index);
 
-		cv::Mat img1_org = cv::imread(img1_path/*, cv::IMREAD_GRAYSCALE*/);
-		cv::Mat img2_org = cv::imread(img2_path/*, cv::IMREAD_GRAYSCALE*/);
+		cv::Mat img1_org = cv::imread(img1_path, cv::IMREAD_GRAYSCALE);
+		cv::Mat img2_org = cv::imread(img2_path, cv::IMREAD_GRAYSCALE);
 
 		if( !img1_org.data )
 		{
@@ -126,16 +132,42 @@ int main( int argc, char **argv )
 			break;
 		}
 
-		// downscale images
-		const double scale_factor = 0.3;
+
+		/*
+		 * rescale images
+		 */
+		const double scale_factor  = 0.3;
+		const double inverse_scale = 1.0 / scale_factor;
 
 		cv::Mat img1;
 		cv::Mat img2;
 
-		cv::resize(img1_org, img1, cv::Size(), scale_factor, scale_factor, cv::INTER_AREA);
-		cv::resize(img2_org, img2, cv::Size(), scale_factor, scale_factor, cv::INTER_AREA);
+		if( scale_factor != 1.0 )
+		{
+			const int interpolation_method = scale_factor < 1.0 ? cv::INTER_AREA : cv::INTER_CUBIC;
 
+			cv::resize(img1_org, img1, cv::Size(), scale_factor, scale_factor, interpolation_method);
+			cv::resize(img2_org, img2, cv::Size(), scale_factor, scale_factor, interpolation_method);
+		}
+		else
+		{
+			img1 = img1_org.clone();
+			img2 = img2_org.clone();
+		}
+
+		// save the rescaled image if desired
+		if( !dataset_out_path.empty() )
+		{
+			char img1_out_path[512];
+			sprintf(img1_out_path, dataset_out_path.c_str(), img1_index);
+
+			if( cv::imwrite(img1_out_path, img1) )
+				cout << "frame #" << img1_index << " saved to " << img1_out_path << endl << endl;
+			else
+				cout << "frame #" << img1_index << " failed to save to " << img1_out_path << endl << endl;
+		}
 	
+
 		/*
 		 * detect keypoints
 		 */
@@ -289,12 +321,15 @@ int main( int argc, char **argv )
 
 		for( uint32_t i=0; i < pts2.size(); i++ )
 		{
-			cout << "  " << pts1[i] << " -> " << pts2[i];
-
 			const double max_displacement = 32.0;
 
 			const double dx = pts2[i].x - pts1[i].x;
 			const double dy = pts2[i].y - pts1[i].y;
+
+			labels_file << dx << ",";
+			labels_file << dy << ",";
+
+			cout << "  " << pts1[i] << " -> " << pts2[i] << "   (dx = " << dx << ") (dy = " << dy << ")";
 
 			if( std::fabs(dx) > max_displacement || std::fabs(dy) > max_displacement )
 				cout << "   (max displacement of " << max_displacement << "px exceeded)";
@@ -302,6 +337,7 @@ int main( int argc, char **argv )
 			cout << endl;
 		}
 
+		labels_file << endl;
 		cout << endl;
 
 
@@ -309,9 +345,7 @@ int main( int argc, char **argv )
 		cv::Mat img1_warped;
 
     		cv::warpPerspective(img1, img1_warped, H, img1.size());
-
-		//cout << "input image  = " << img1.rows << "x" << img1.cols << endl;
-		//cout << "warped image = " << img1_warped.rows << "x" << img1_warped.cols << endl;
+		cv::cvtColor(img1_warped, img1_warped, cv::COLOR_GRAY2BGR);
 
 
 		/*
@@ -323,6 +357,17 @@ int main( int argc, char **argv )
 		// draw the keypoints
 		cv::drawKeypoints(img1, img1_inlier_keypoints, img1_overlay, cvScalar(0,255,0));
 		cv::drawKeypoints(img2, img2_inlier_keypoints, img2_overlay, cvScalar(0,255,0));
+
+		// rescale images to original size
+		if( scale_factor != 1.0 )
+		{
+			const int interpolation_method = inverse_scale < 1.0 ? cv::INTER_AREA : cv::INTER_CUBIC;
+
+			cv::resize(img1_overlay, img1_overlay, cv::Size(), inverse_scale, inverse_scale, interpolation_method);
+			cv::resize(img2_overlay, img2_overlay, cv::Size(), inverse_scale, inverse_scale, interpolation_method);
+
+			cv::resize(img1_warped, img1_warped, cv::Size(), inverse_scale, inverse_scale, interpolation_method);
+		}
 
 		// apply some overlay text to the images
 		char img_label[128];
@@ -350,256 +395,11 @@ int main( int argc, char **argv )
 		cv::imshow("Keypoints", img_overlay_composted);
 		cv::waitKey(/*1*/);
 
-		/*
-		 * extract descriptors
-		 */
-		//feature_detector.
+		// increment next frame index
 		img_index++;
 	}
 
-#if 0
-	/*
-	 * open the camera pose file
-	 */
-	std::ifstream pose_file(pose_path, std::ifstream::in);
-	
-	if( !pose_file.is_open() )
-	{
-		cout << "failed to open pose file:  " << pose_path << endl;
-		return -1;
-	}
-
-
-	/*
-	 * parse the camera poses
-	 */
-	std::vector<cv::Mat> poses;
-
-	for( std::string line; std::getline(pose_file, line); )
-	{
-		//cout << line << endl;
-
-		std::stringstream parser;
-		parser << line;
-
-		cv::Mat cam_pose = cv::Mat::zeros(3, 4, CV_64FC1);
-
-		for( int y=0; y < 3; y++ )
-		{
-			for( int x=0; x < 4; x++ )
-			{
-				double value = 0.0;
-				parser >> value;
-
-				if( (parser.rdstate() & std::ifstream::failbit) != 0 )
-				{
-					cout << "parser error" << endl;
-					continue;
-				}
-
-				cam_pose.at<double>(y,x) = value;
-			}
-		}
-
-		//cout << endl << "pose[" << poses.size() << "] = " << endl << " " << cam_pose << endl << endl;
-		poses.push_back(cam_pose);
-
-		//if( poses.size() >= 3 )
-		//	break;	
-	}
-
-	pose_file.close();
-
-	const uint32_t numPoses = poses.size();	
-	cout << "parsed " << numPoses << " camera poses" << endl << endl;
-	
-	if( numPoses == 0 )
-	{
-		cout << "error:  zero camera poses were parsed" << endl;
-		return -1;
-	}
-
-	
-	/*
-	 * process each frame
-	 */	
-	const uint32_t framesAhead = 2;	// the number of frames ahead to compute the homography
-
-	std::vector<uint32_t> outlierFrames;
-	const double outlierThreshold = 15.0;
-
-	for( uint32_t n=0; n < numPoses-framesAhead; n++ )
-	{
-		cout << "-----------------------------------------" << endl;
-		cout << "-- FRAME " << n << endl;
-		cout << "-----------------------------------------" << endl;
-
-		// extract rotation/translation from frame #1
-		cv::Mat R1(3, 3, CV_64FC1);
-		cv::Mat T1(3, 1, CV_64FC1);
-
-		extractRotation(poses[n], R1);
-		extractTranslation(poses[n], T1);
-
-		cout << "R1 = " << endl << " " << R1 << endl << endl;
-		cout << "T1 = " << endl << " " << T1 << endl << endl;
-
-		// extract rotation/translation from frame #2
-		cv::Mat R2(3, 3, CV_64FC1);
-		cv::Mat T2(3, 1, CV_64FC1);
-
-		extractRotation(poses[n+framesAhead], R2);
-		extractTranslation(poses[n+framesAhead], T2);
-
-		cout << "R2 = " << endl << " " << R2 << endl << endl;
-		cout << "T2 = " << endl << " " << T2 << endl << endl;
-
-		// compute camera displacement
-		cv::Mat R_1to2;
-		cv::Mat T_1to2;
-
-		computeC2MC1(R1, T1, R2, T2, R_1to2, T_1to2);
-
-		//cout << "R_1to2 rows: " << R_1to2.rows << " cols: " << R_1to2.cols << endl;
-		//cout << "T_1to2 rows: " << T_1to2.rows << " cols: " << T_1to2.cols << endl << endl;
-
-		cout << "R_1to2 = " << endl << " " << R_1to2 << endl << endl;
-		cout << "T_1to2 = " << endl << " " << T_1to2 << endl << endl;
-
-		// compute plane normal @ camera #1
-		cv::Mat normal  = (cv::Mat_<double>(3,1) << 0, 0, 1);
-    		cv::Mat normal1 = R1*normal;
-
-		cout << "normal1 = " << endl << " " << normal1 << endl << endl;
-
-		// compute plane distance to camera frame #1
-		cv::Mat origin(3, 1, CV_64F, cv::Scalar(0));
-		cv::Mat origin1 = R1 * origin + T1;
-
-		cout << "origin1 = " << endl << " " << origin1 << endl << endl;
-
-		const double d_inv1 = 1.0 / normal1.dot(origin1);
-		cout << "d_inv1 = " << d_inv1 << endl << endl;
-
-		if( std::fabs(d_inv1) >= outlierThreshold )
-			outlierFrames.push_back(n);
-
-		// compute homography from camera displacement
-		cv::Mat homography_euclidean = computeHomography(R_1to2, T_1to2, d_inv1, normal1);
-		cv::Mat homography = instrinsic_mat * homography_euclidean * instrinsic_mat.inv();
-
-		//cout << "homography_euclidean = " << endl << " " << homography_euclidean << endl << endl;
-		//cout << "homography = " << endl << " " << homography << endl << endl;
-
-#if 1		
-		homography_euclidean /= homography_euclidean.at<double>(2,2);
-		homography /= homography.at<double>(2,2);
-#endif
-		//cout << "homography_euclidean = " << endl << " " << homography_euclidean << endl << endl;
-		cout << "homography = " << endl << " " << homography << endl << endl;
-		
-		// same, but using absolute camera poses instead of camera displacement, just for check
-#if 0
-		cv::Mat homography_euclidean2 = computeHomography(R1, T1, R2, T2, d_inv1, normal1);
-		cv::Mat homography2 = instrinsic_mat * homography_euclidean2 * instrinsic_mat.inv();
-
-		cout << "homography_euclidean2 = " << endl << " " << homography_euclidean2 << endl << endl;
-		cout << "homography2 = " << endl << " " << homography2 << endl << endl;
-		
-		homography_euclidean2 /= homography_euclidean2.at<double>(2,2);
-		homography2 /= homography2.at<double>(2,2);
-		
-		cout << "homography_euclidean2 = " << endl << " " << homography_euclidean2 << endl << endl;
-		cout << "homography2 = " << endl << " " << homography2 << endl << endl;
-#endif
-
-		// test the homography transform
-		const int img_width  = 1241;
-		const int img_height = 376;
-
-		std::vector<cv::Point2f> pts1;
-		std::vector<cv::Point2f> pts2;
-
-		pts1.resize(4);
-		pts2.resize(4);
-
-		pts1[0].x = 0.0f;       pts1[0].y = 0.0f;
-		pts1[1].x = img_width;  pts1[1].y = 0.0f;
-		pts1[2].x = img_width;  pts1[2].y = img_height;
-		pts1[3].x = 0.0f;       pts1[3].y = img_height;
-
-		cv::perspectiveTransform(pts1, pts2, homography);
-
-		cout << "Corner points transformed by homography = " << endl;
-
-		for( uint32_t i=0; i < pts2.size(); i++ )
-			cout << "  " << pts1[i] << " -> " << pts2[i] << endl;
-
-		cout << endl;
-
-
-		// load the images for visualization
-		char img1_path[512];
-		char img2_path[512];
-
-		sprintf(img1_path, "%s/sequences/%s/image_0/%06u.png", dataset_path.c_str(), sequence.c_str(), n);
-		sprintf(img2_path, "%s/sequences/%s/image_0/%06u.png", dataset_path.c_str(), sequence.c_str(), n+framesAhead);
-		
-		cv::Mat img1 = cv::imread(img1_path);
-		cv::Mat img2 = cv::imread(img2_path);
-
-		if( !img1.data )
-		{
-			cout << "failed to load image " << img1_path << endl;
-			continue;
-		}
-
-		if( !img2.data )
-		{
-			cout << "failed to load image " << img1_path << endl;
-			continue;
-		}
-
-		// warp the input image by the homography
-		cv::Mat img1_warp;
-    		cv::warpPerspective(img1, img1_warp, homography.inv(), img1.size());
-	
-		// apply some overlay text to the images
-		char img_label[128];
-
-		sprintf(img_label, "Frame #%u", n);
-
-		cv::putText(img1, img_label, cvPoint(10,40), 
-    				  cv::FONT_HERSHEY_SIMPLEX, 1.5, cvScalar(0,175,255), 3/*, CV_AA*/);
-
-		sprintf(img_label, "Frame #%u", n+framesAhead);
-
-		cv::putText(img2, img_label, cvPoint(10,40), 
-    				  cv::FONT_HERSHEY_SIMPLEX, 1.5, cvScalar(0,175,255), 3/*, CV_AA*/);
-
-		cv::putText(img1_warp, "Warped", cvPoint(10,40), 
-    				  cv::FONT_HERSHEY_SIMPLEX, 1.5, cvScalar(0,175,255), 3/*, CV_AA*/);
-
-		// display the warped images
-		cv::Mat img_compare;
-
-		cv::hconcat(img1, img2, img_compare);
-		cv::hconcat(img_compare, img1_warp, img_compare);
-		cv::imshow("Comparision of camera image warped by homography", img_compare);
-		cv::waitKey(/*1*/);
-	}
-
-
-	// print out the outlier frames
-	cout << "number of outlier frames:   " << outlierFrames.size() << endl;
-	cout << "indices of outlier frames:  ";
-
-	for( uint32_t n=0; n < outlierFrames.size(); n++ )
-		cout << outlierFrames[n] << " ";
-
-	cout << endl;
-#endif
-
+	labels_file.close();
 	return 0;
 }
 
